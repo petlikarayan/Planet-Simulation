1e8623d2283cb89e7331ad73008b95d68e51b2c7
petlikarayan <petlikarayan@gmail.com>
2022-02-22 22:41:22 -0500

    Planets orbiting the sun

--- main.py
+++ main.py
@@ -35,6 +35,39 @@ class Planet:
         pygame.gfxdraw.aacircle(win, int(x), int(y), self.radius, self.colour)
         pygame.gfxdraw.filled_circle(win, int(x), int(y), self.radius, self.colour)
 
+    def attraction(self, planet):
+        planetX, planetY = planet.x, planet.y
+        distanceX = planetX - self.x
+        distanceY = planetY - self.y
+        distance = math.sqrt(distanceX**2 + distanceY**2)
+        if planet.sun:
+            self.distanceToSun = distance
+
+        force = (self.G * self.mass * planet.mass) / distance**2
+        theta = math.atan2(distanceY, distanceX)
+        forceX = math.cos(theta) * force
+        forceY = math.sin(theta) * force
+
+        return forceX, forceY
+
+    def updatePos(self, planets):
+        totalFx = totalFy = 0
+
+        for planet in planets:
+            if self == planet:
+                continue
+
+            fX, fY = self.attraction(planet)
+            totalFx += fX
+            totalFy += fY
+
+        self.xVel += totalFx / self.mass * self.TIMESTEP
+        self.yVel += totalFy / self.mass * self.TIMESTEP
+
+        self.x += self.xVel * self.TIMESTEP
+        self.y += self.yVel * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
+
 def main():
     run = True
     planets = []
@@ -45,24 +78,30 @@ def main():
     planets.append(sun)
 
     earth = Planet(-1 * Planet.AU, 0, 16, (100, 149, 237), 5.9742 * 10**24)
+    earth.yVel = 29.783 * 1000
     planets.append(earth)
     
     mars = Planet(-1.524 * Planet.AU, 0, 12, (207, 119, 87), 6.39 * 10**23)
+    mars.yVel = 24.077 * 1000
     planets.append(mars)
 
     mercury = Planet(0.387 * Planet.AU, 0, 8, (80, 78, 81), 3.30 * 10**23)
+    mercury.yVel = -47.4 * 1000
     planets.append(mercury)
 
     venus = Planet(0.723 * Planet.AU, 0, 14, (255, 255, 255), 4.8685 * 10**24)
+    venus.yVel = -35.02 * 1000
     planets.append(venus)
 
     while run:
         clock.tick(60)
+        WIN.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         for planet in planets:
+            planet.updatePos(planets)
             planet.draw(WIN)
         
         pygame.display.update()
